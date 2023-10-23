d9925b8a8a3067e9a511d97503408be77b3c5eb5
Toga280 <tristan.ggouveia.fr@gmail.com>
2023-10-23 15:10:08 +0200

    tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>yee</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
