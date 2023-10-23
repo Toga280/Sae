1df033567a77fd26a8631931db9cdff01e49d558
chihiroprog <stutuart.barthe@gmail.com>
2023-10-23 16:06:30 +0200

    fichebox1

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,17 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import FicheBox1 from "./Component/FicheBox1";
-<<<<<<< HEAD
-ReactDOM.render(
-  <React.StrictMode>
-    <FicheBox1></FicheBox1>
-=======
 
 import FicheBox3 from "./Component/FicheBox3";
 import FicheBox4 from "./Component/FicheBox4";
 import FicheBox5 from "./Component/FicheBox5";
-import FicheBox6 from "./Component/FicheBox6";
-import FicheBox7 from "./Component/FicheBox7";
+// import FicheBox6 from "./Component/FicheBox6";
+// import FicheBox7 from "./Component/FicheBox7";
 
 
 ReactDOM.render(
@@ -21,9 +16,8 @@ ReactDOM.render(
     <FicheBox3 />
     <FicheBox4 />
     <FicheBox5 />
-    <FicheBox6 />
-    <FicheBox7 />
->>>>>>> 437f77b663f1a571b921f8e4beaa6ca2a08b66c3
+    {/* <FicheBox6 />
+    <FicheBox7 /> */}
   </React.StrictMode>,
   document.getElementById("root")
 );
