860b0a08bfc3172c1d7c257236e848d465831173
REN-22 <renzo.bastide@gmail.com>
2023-10-27 14:31:42 +0200

    add FicheBoxTotal

---
+++ frontend/src/Component/FicheBoxTotal.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import FicheBox1 from "./FicheBox1";
+import FicheBox2 from "./FicheBox2";
+import FicheBox3 from "./FicheBox3";
+import FicheBox4 from "./FicheBox4";
+import FicheBox5 from "./FicheBox5";
+import FicheBox6 from "./FicheBox6";
+import FicheBox7 from "./FicheBox7";
+import FicheBox8 from "./FicheBox8";
+
+function FicheBoxTotal() {
+    return (
+        <div>
+            <FicheBox1 />
+            <FicheBox2 />
+            <FicheBox3 />
+            <FicheBox4 />
+            <FicheBox5 />
+            <FicheBox6 />
+            <FicheBox7 />
+            <FicheBox8 />
+        </div>
+    );
+}
+
+export default FicheBoxTotal;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,27 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import FicheBox1 from "./Component/FicheBox1";
-import FicheBox2 from "./Component/FicheBox2";
-import FicheBox3 from "./Component/FicheBox3";
-import FicheBox4 from "./Component/FicheBox4";
-import FicheBox5 from "./Component/FicheBox5";
-import FicheBox6 from "./Component/FicheBox6";
-import FicheBox7 from "./Component/FicheBox7";
-import FicheBox8 from "./Component/FicheBox8";
-
-
+import FicheBoxTotal from "./Component/FicheBoxTotal";
 
 ReactDOM.render(
   <React.StrictMode>
-    <FicheBox1 />
-    <FicheBox2 />
-    <FicheBox3 />
-    <FicheBox4 />
-    <FicheBox5 />
-    <FicheBox6 />
-    <FicheBox7 />
-    <FicheBox8 />
+    <FicheBoxTotal />
   </React.StrictMode>,
   document.getElementById("root")
 );
