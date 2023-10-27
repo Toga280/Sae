d5cce9f18b08e29148a8a065fab8269698145119
chihiroprog <stutuart.barthe@gmail.com>
2023-10-27 14:49:50 +0200

    clean Box7

--- frontend/src/Component/FicheBox7.tsx
+++ frontend/src/Component/FicheBox7.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 
 function FicheBox7() {
     return (
-        <div>
-            
-                <div className="titre demi">Travaux réalisés</div>
-                <div>
-                    <textarea rows={10}></textarea>
-                </div>
-
-            <div className="espaceHor"></div>
-
+        <div>     
+            <div className="">Travaux réalisés</div>
+            <textarea rows={10}></textarea>
             <div>
-                <div className="colonnePrint">
-                    <div className="titre demi">Travaux non réalisés</div>
-                    <div>
-                        <textarea rows={5}></textarea>
-                    </div>
+                <div className="">Travaux non réalisés</div>
+                <div>
+                    <textarea rows={5}></textarea>
                 </div>
             </div>
         </div>
