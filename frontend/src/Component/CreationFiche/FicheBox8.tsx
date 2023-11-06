a99c3ba0bb47471c37abf25198e66aa73d08cf0e
chihiroprog <stutuart.barthe@gmail.com>
2023-11-06 16:36:58 +0100

    fichebox8

--- frontend/src/Component/CreationFiche/FicheBox8.tsx
+++ frontend/src/Component/CreationFiche/FicheBox8.tsx
@@ -1,88 +1,25 @@
 import React from 'react';
-
+import DupliquerSelect from './DupliquerSelect';
 function FicheBox8() {
     return (
 
-        <><div className="Box">
 
-
-            <input type="checkbox" /> Nécessite une nouvelle intervention
-
-            <div className="">Matériaux Utilisés</div>
-            <div >
-                {/* Liste des matériaux */}
-                <select>
-                    <option>-- Choisir une valeur --</option>
-                    <option>Bonde à grille pour lave-mains</option>
-                    <option>Bouchon laiton à visser F 1/2</option>
-                    <option>Chevilles à expansion avec patte à vis</option>
-                    <option>Chevilles à frapper</option>
-                    <option>Chevilles autoforeuses - Fixation plaque de plâtre</option>s
-                    <option>Chiffons</option>
-                    <option>Colle acrylique de fixation pour plinthe</option>
-                    <option>Colle PVC</option>
-                    <option>Collier PVC Ø 40</option>
-                    <option>Colliers PVC Ø 32</option>
-                    <option>Colliers PVC  Ø 100</option>
-                    <option>Colliers type Atlas double Ø12</option>
-                    <option>Colliers type Atlas Simple Ø12</option>
-                    <option>Coude cuivre 90° à souder FF Ø 12</option>
-                    <option>Coude PVC 87°30° FF Ø 100</option>
-                    <option>Coude PVC 87°30° FF Ø 32</option>
-                    <option>Coude PVC 87°30° FF Ø 40</option>
-                    <option>Faïence mur 20 x 20</option>
-                    <option>Mortier colle poudre</option>
-                    <option>Joint poudre carrelage</option>
-                    <option>Ecrou laiton à collet battu 12x17 Ø 12</option>
-                    <option>Joints d'étanchéité suivant montages</option>
-                    <option>Kit robinet d'arrêt WC équerre + flexible + joint</option>
-                    <option>Lave-mains</option>
-                    <option>Lot de 2 chevilles clips pour fixation WC 6x70</option>
-                    <option>Lot de 2 fixations pour lave-mains parois creuse + cheville</option>
-                    <option>Lot de 2 fixations pour lave-mains parois pleine + cheville</option>
-                    <option>Manchon cuivre à souder FF Ø 12</option>
-                    <option>Manchon de dilatation PVC H Ø 100</option>
-                    <option>Manchon mâle 243 CGU Ø 12 - M 12x17</option>
-                    <option>Manchon mâle 243 CGU Ø 12 - M 15x21</option>
-                    <option>Manchon PVC Ø 100</option>
-                    <option>Manchon PVC Ø 32</option>
-                    <option>Manchon PVC Ø 40</option>
-                    <option>Mélangeur pour lave-mains + Fléxibles de raccordement</option>
-                    <option>Pack WC à poser sortie horizontale</option>
-                    <option>Panneau bois (OSB ou aggloméré)</option>
-                    <option>Papier de verre grain 120</option>
-                    <option>Papier de verre grain 80</option>
-                    <option>Pates à vis</option>
-                    <option>Pipe coudée WC 90° 110 mm</option>
-                    <option>Pipe droite WC 110 mm</option>
-                    <option>Planche de coffrage</option>
-                    <option>Réduction PVC Ø 40/32</option>
-                    <option>Robinet de puisage de lave-linge + platine de fixation</option>
-                    <option>Robinet simple pour lave-mains + fléxible de raccordement</option>
-                    <option>Rosaces coniques H19</option>
-                    <option>Siphon lavabo/lave-mains à visser sortie Ø 32</option>
-                    <option>Système de vidage PVC pour machine à laver</option>
-                    <option>Tampon de réduction simple PVC Ø 100/40</option>
-                    <option>Tampon de visite PVC avec bouchon M/F Ø 100</option>
-                    <option>Tampon de visite PVC avec bouchon M/F Ø 32</option>
-                    <option>Té égal cuivre à souder FFF Ø 12</option>
-                    <option>Té pied de biche 87°30 FF PVC Ø 100</option>
-                    <option>Té pied de biche 87°30 FF PVC Ø 32</option>
-                    <option>Té pied de biche 87°30 FF PVC Ø 40</option>
-                    <option>Tube cuivre Ø 12</option>
-                    <option>Tube PVC Ø 100</option>
-                    <option>Tube PVC Ø 32</option>
-                    <option>Tube PVC Ø 40</option>
-                    <option>Vanne d'arrêt MF 1/4 de tour - 12x17</option>
-                    <option>Verrou à bouton - cylindre 40 mm</option>
-                    <option>Vis à bois 30 mm</option>
-                    <option>Vis TRPF</option>
-                    <option>Vis TTPC 25</option>
-                    <option>Vis TTPC 35</option>
-                </select>
+        <div className="Box">
+        <input type="checkbox" /> Nécessite une nouvelle intervention
+            <div className='SelectMateriaux'>
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
+                <DupliquerSelect />
             </div>
         </div>
-        </>
+
     );
 }
 
