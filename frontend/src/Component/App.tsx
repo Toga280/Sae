3698da764b9a0a019593349b588bdf7891e77329
Toga280 <tristan.ggouveia.fr@gmail.com>
2023-11-06 16:59:25 +0100

    app and ficheBoxTotal

--- frontend/src/Component/App.tsx
+++ frontend/src/Component/App.tsx
@@ -2,8 +2,21 @@ import React, { useState } from "react";
 import FicheBoxTotal from "./CreationFiche/FicheBoxTotal";
 
 function App() {
-  const [];
-  return <FicheBoxTotal />;
+  const [selectBox, setSelectBox] = useState(null);
+
+  const handleSelectBoxChange = (value: any) => {
+    setSelectBox(value);
+  };
+
+  return (
+    <div>
+      {selectBox === null ? (
+        <FicheBoxTotal onSelectBox={handleSelectBoxChange} />
+      ) : (
+        <p> ok </p>
+      )}
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/Component/CreationFiche/FicheBoxTotal.tsx
+++ frontend/src/Component/CreationFiche/FicheBoxTotal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FicheBox1 from "./FicheBox1";
 import FicheBox2 from "./FicheBox2";
 import FicheBox3 from "./FicheBox3";
@@ -7,32 +7,64 @@ import FicheBox5 from "./FicheBox5";
 import FicheBox6 from "./FicheBox6";
 import FicheBox7 from "./FicheBox7";
 import FicheBox8 from "./FicheBox8";
+function FicheBoxTotal({ onSelectBox }: any) {
+  const [numBox, setNumBox] = useState(0);
+  const handleClick1 = () => {
+    setNumBox(1);
+  };
+  const handleClick2 = () => {
+    setNumBox(2);
+  };
+  const handleClick3 = () => {
+    setNumBox(3);
+  };
+  const handleClick4 = () => {
+    setNumBox(4);
+  };
+  const handleClick5 = () => {
+    setNumBox(5);
+  };
+  const handleClick6 = () => {
+    setNumBox(6);
+  };
+  const handleClick7 = () => {
+    setNumBox(7);
+  };
+  const handleClick8 = () => {
+    setNumBox(8);
+  };
 
-function FicheBoxTotal() {
+  useEffect(() => {
+    if (numBox !== 0) {
+      onSelectBox(numBox);
+      console.log(numBox);
+    }
+  }, [numBox, onSelectBox]);
+
   return (
     <div>
-      <div>
+      <div onClick={handleClick1}>
         <FicheBox1 />
       </div>
-      <div>
-        <FicheBox2 /> 
+      <div onClick={handleClick2}>
+        <FicheBox2 />
       </div>
-      <div>
+      <div onClick={handleClick3}>
         <FicheBox3 />
       </div>
-      <div>
+      <div onClick={handleClick4}>
         <FicheBox4 />
       </div>
-      <div>
+      <div onClick={handleClick5}>
         <FicheBox5 />
       </div>
-      <div>
+      <div onClick={handleClick6}>
         <FicheBox6 />
       </div>
-      <div>
+      <div onClick={handleClick7}>
         <FicheBox7 />
       </div>
-      <div>
+      <div onClick={handleClick8}>
         <FicheBox8 />
       </div>
     </div>
