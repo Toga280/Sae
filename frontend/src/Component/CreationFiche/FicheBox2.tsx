09239c2a39bcc487cb389cbd0ae3993e61a44438
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-11-06 16:59:34 +0100

    css avancement

--- frontend/src/Component/CreationFiche/FicheBox2.tsx
+++ frontend/src/Component/CreationFiche/FicheBox2.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import "../../style/fiche2.css"
+import "../../style/ficheGlobal.css"
 
 function FicheBox2() {
     return (
         <div className='Box'>
-                <div>
+                <div className='fb2'>
                     <div className="nom_inter">Nom de l'intervenant</div>
                     <div>
                         <input type="text" />
--- frontend/src/Component/CreationFiche/FicheBox3.tsx
+++ frontend/src/Component/CreationFiche/FicheBox3.tsx
@@ -1,28 +1,30 @@
 import React from 'react';
+import "../../style/fiche3.css"
+import "../../style/ficheGlobal.css"
 
 function FicheBox3() {
     return (
-            <div className="Box">
-                <div>
-                    <div className="titre">Nom du demandeur</div>
-                    <div> <input value=""/></div>  
-                </div>
-                <div>
-                    <div className="">Date de la demande</div>
-                        <input value=""/>
-                    <div className="">Localisation</div>
-                        <input value=""/>
-                </div>
-                <div className="">
-                    <div className="">Description de la demande</div>
+            <div className='Box'>
+                <div className='flex'>
+                    <div className='grp_demandeur'>
+                        <div className='name_demandeur'>Nom du demandeur</div>
+                        <div> <input value=""/></div>  
+                    </div>
+                    <div className='grp_localisation'>
+                        <div className="date_localisation">Date de la demande</div>
+                            <input value=""/>
+                        <div className="locali_localisation">Localisation</div>
+                            <input value=""/>
+                    </div>
+                    <div className="description_demande">Description de la demande</div>
                     <div> 
                         <textarea rows={10}>
                         </textarea>
                     </div>
-                </div>
-                <div>
-                    <div className="">Degré d'urgence</div>
-                    <div> <input value=""/></div>
+                    <div>
+                        <div className="">Degré d'urgence</div>
+                        <div> <input value=""/></div>
+                    </div>
                 </div>
             </div>
     );
