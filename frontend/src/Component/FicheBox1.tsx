cf64ea14e68ffa2f1ffcc7c3acc23605e64c297d
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-11-06 14:33:55 +0100

    style

--- frontend/src/Component/FicheBox1.tsx
+++ frontend/src/Component/FicheBox1.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react';
+import '../style/fiche.css';
 
 function FicheBox1() {
     let numeroFiche : number = 10
 
     return (
-        <div className="Box">
+        <div className="boxes">
             <h1>Fiche d'intervention N°{numeroFiche}</h1> 
         </div>
     );
