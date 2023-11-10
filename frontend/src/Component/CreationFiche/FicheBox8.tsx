6256806017406e4ecd9196ed94c43d0ab095ccb8
chihiroprog <stutuart.barthe@gmail.com>
2023-11-10 11:54:33 +0100

    add file

--- frontend/src/Component/CreationFiche/FicheBox8.tsx
+++ frontend/src/Component/CreationFiche/FicheBox8.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import DupliquerSelect from './DupliquerSelect';
+import "../../style/ficheGlobal.css";
+import "../../style/fiche8.css"
 function FicheBox8() {
     return (
 
-
         <div className="Box">
         <input type="checkbox" /> Nécessite une nouvelle intervention
+
             <div className='SelectMateriaux'>
+            Matériaux Utilisés
                 <DupliquerSelect />
                 <DupliquerSelect />
                 <DupliquerSelect />
