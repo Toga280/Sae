03dbfb86ec98d5c712dbe39fafe047ee05ca9769
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-11-20 15:25:30 +0100

    connection

--- frontend/src/Component/App.tsx
+++ frontend/src/Component/App.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import page_connection from "./ConnectionPage/pageConnection";
 import FicheBoxTotal from "./CreationFiche/FicheBoxTotal";
 import FicheBoxSelected from "./CreationFiche/FicheBoxSelected";
+import PageConnection from "./ConnectionPage/pageConnection";
+
 function App() {
+  /*
   const [selectBox, setSelectBox] = useState(null);
 
   const handleSelectBoxChange = (value: any) => {
     setSelectBox(value);
   };
-
+  */
   return (
+    /*
     <div>
       {selectBox === null ? (
         <FicheBoxTotal onSelectBox={handleSelectBoxChange} />
@@ -19,6 +24,8 @@ function App() {
         />
       )}
     </div>
+*/
+    <PageConnection />
   );
 }
 
