9b36e140026726c290b9b7d42b18a22a13c52f56
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-12-11 10:55:07 +0100

    fix-back-co-elelve

--- frontend/src/Component/ConnectionPage/ConnectionEleveShema.tsx
+++ frontend/src/Component/ConnectionPage/ConnectionEleveShema.tsx
@@ -1,10 +1,36 @@
 import React, { useState } from "react";
 import "./connectionEleveShema.css";
-function ConnectionEleveShema({ redirection }: any){
-  
-  const redirectionFour = () => {
-      redirection(4);
+import axios from "axios";
+function ConnectionEleveShema({ redirection, setC, nomEleveActuelle, prenomEleveActuelle }: any) {
+  const [mdpFaux, setMdpFaux] = useState(false);
+
+  const Connexion = (event: React.FormEvent) => {
+    event.preventDefault();
+    setMdpFaux(false);
+
+    axios
+      .get(
+        `http://localhost:5000/GET/eleve/authentification?prenom=${encodeURIComponent(
+          prenomEleveActuelle
+        )}&nom=${encodeURIComponent(nomEleveActuelle)}&mdp=${encodeURIComponent(password)}`
+      )
+      .then((response) => {
+        console.log("Réponse du serveur :", response.data);
+        if (response.data) {
+          redirection(4);
+        } else {
+          setMdpFaux(true);
+        }
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 401) {
+          setMdpFaux(true);
+        } else {
+          console.error("Erreur lors de la requête :", error);
+        }
+      });
   };
+
 
   const [password, setPassword] = useState(Number);
   const [numberPos, setNumberPos] = useState(Number);
@@ -53,6 +79,7 @@ function ConnectionEleveShema({ redirection }: any){
 
   return (
     <div className="container">
+        <p className="affichageMDP">{mdpFaux ? "Mot de passe incorrect" : null}</p>
         <div className="affichage">
         <p className="affichageMDP">{password !== 0 ? (password) : null }</p> 
         </div>
@@ -69,7 +96,10 @@ function ConnectionEleveShema({ redirection }: any){
                     <button className="bouton" onClick={() => addNumber(9)}>9</button>
                     <button className="bouton" onClick={() => removeNumber()}> <img src = {require("../CreationFiche/MiniBoxChoix/imagesTestStuart/retour.png")} alt="suprimmer chiffre" className="btn-retour-pin"></img> </button>
                     <button className="bouton" onClick={() => addNumber(0)}>0</button>
-                    <button className="bouton" onClick={() => submbitPassword()}>Suivant</button>
+                    <button className="bouton" onClick={(e) => Connexion(e)}>Connexion</button>
+                    <button className="bouton_retour_connection_eleve" onClick={() => setC(false)}>
+                    Retour
+                  </button>
                 </div>  
             </div>
     </div>
