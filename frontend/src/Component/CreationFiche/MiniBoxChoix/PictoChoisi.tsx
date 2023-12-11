7c8cdc81b54262c1bc3b5b27843960dc4a3fdd68
REN-22 <renzo.bastide@gmail.com>
2023-12-11 16:52:02 +0100

    C presque plus PT

---
+++ frontend/src/Component/CreationFiche/MiniBoxChoix/PictoChoisi.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import fonctionsMiniBoxInfoJson from "../MiniBoxInfoFunction";
+
+function PictoChoisi({ numeroMiniBox }: any) {
+    const [imagePath, setImagePath] = useState<string | null>(null);
+    useEffect(() => {
+        const getPictoFile = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/GET/getpicto-file', {
+                    params: {
+                        name: fonctionsMiniBoxInfoJson.getNomPicto(numeroMiniBox)
+                    },
+                    responseType: 'blob',
+                });
+
+                
+                const imageUrl = URL.createObjectURL(response.data);
+
+                setImagePath(imageUrl);
+            } catch (error) {
+                console.error('Erreur lors de la récupération du fichier image :', error);
+            }
+        };
+
+        getPictoFile();
+    }, [numeroMiniBox]);
+
+    return (
+        <div>
+            {imagePath && (
+                <img
+                    src={imagePath}
+                    alt="pictogramme"
+                    style={{ maxWidth: '200px', maxHeight: '200px' }}
+                />
+            )}
+        </div>
+    );
+}
+
+export default PictoChoisi;
