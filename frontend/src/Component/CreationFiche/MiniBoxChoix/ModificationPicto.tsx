59e69b5dda5faba726933deeb1da98a7b39a0f5a
REN-22 <renzo.bastide@gmail.com>
2023-12-11 11:59:28 +0100

    CPT

--- frontend/src/Component/CreationFiche/MiniBoxChoix/ModificationPicto.tsx
+++ frontend/src/Component/CreationFiche/MiniBoxChoix/ModificationPicto.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Pictogramme from './Pictogramme';
-function ModificationPicto({ setModificationPictoPropsFalse }: any){
- return (
-    <div className="select_all_choix_picto">
-        <p>Choix du pictogramme : </p>
-        <div onClick={setModificationPictoPropsFalse}>
-            <Pictogramme />
-        </div>
-        <div onClick={setModificationPictoPropsFalse}> 
-            <Pictogramme />
-        </div>
-    </div>
- )
 
+function ModificationPicto({ setModificationPictoPropsFalse }: any) {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const handleSelect = (image: string) => {
+    setSelectedImage(image);
+  };
+
+  return (
+    <div className="select_all_choix_picto">
+      <p>Choix du pictogramme : </p>
+      <div onClick={setModificationPictoPropsFalse}>
+        <Pictogramme onSelect={handleSelect} />
+      </div>
+      <p>Image sélectionnée : {selectedImage}</p>
+    </div>
+  );
 }
 
 export default ModificationPicto;
