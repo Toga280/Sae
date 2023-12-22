0a46ce7433d8180fc6226e9f56d64fc5f653c107
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2023-12-22 14:34:29 +0100

    add

---
+++ frontend/src/Component/Interface_Educateur/ConsulterFichesImages/ConsulterFichesImages.tsx
@@ -0,0 +1,67 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import "../../FicheCreer/AffecterListe.css";
+function ConsulterFichesImages({ redirection }: any) {
+    const setRedirectionTwo = () => {
+        redirection(2);
+      };
+    
+      const [eleves, setEleves] = useState<any[]>([]);
+      const [eleveSelectionne, setEleveSelectionne] = useState<any | null>(null);
+    
+      useEffect(() => {
+        const getEleve = () => {
+          axios
+            .get(`http://localhost:5000/GET/allEleve`)
+            .then((response) => {
+              setEleves(response.data);
+            })
+            .catch((error) => {
+              console.error("erreur : ", error);
+            });
+        };
+    
+        getEleve();
+      }, []);
+    
+      const handleSelectionEleve = (eleve: any) => {
+        setEleveSelectionne(eleve);
+      };
+    
+      return (
+        <div className="global_affecter_fiche">
+          <h1 className="titleh1">Consulter Fiches/Images d'un élève</h1>
+          <h2 className="title">Liste des élèves</h2>
+          <ul>
+            {eleves.map((eleve) => (
+              <li
+                key={eleve.id}
+                onClick={() => handleSelectionEleve(eleve)}
+                style={{
+                  cursor: "pointer",
+                  fontWeight: eleve === eleveSelectionne ? "bold" : "normal",
+                }}
+              >
+                {eleve.nom} {eleve.prenom}
+              </li>
+            ))}
+          </ul>
+          <div>
+            {eleveSelectionne && (
+              <div>
+                <h3>Élève sélectionné :</h3>
+                <p>
+                  {eleveSelectionne.nom} {eleveSelectionne.prenom}
+                </p>
+                <button className="affecter_fiche_eleve">Affecter</button>
+              </div>
+            )}
+          </div>
+          <button className="retour_liste_fiches" onClick={setRedirectionTwo}>
+            Retour
+          </button>
+        </div>
+      );
+    }
+
+export default ConsulterFichesImages;
