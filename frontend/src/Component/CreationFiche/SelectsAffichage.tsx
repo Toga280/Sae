7795ee854e7ba01a2aa581607c0cf53668991917
chihiroprog <stutuart.barthe@gmail.com>
2024-01-08 16:09:27 +0100

    small

--- frontend/src/Component/CreationFiche/SelectsAffichage.tsx
+++ frontend/src/Component/CreationFiche/SelectsAffichage.tsx
@@ -16,13 +16,17 @@ function SelectsAffichage({setSelect, setNumMat} : any) {
 
   return (
 
-    <div className="unSelect">
+    <div className="lesSelect">
         <div onClick={() => setSelectTrue("Mat1")}> {value("Mat1")} </div>
         <div onClick={() => setSelectTrue("Mat2")}> {value("Mat2")} </div>
         <div onClick={() => setSelectTrue("Mat3")}> {value("Mat3")} </div>
         <div onClick={() => setSelectTrue("Mat4")}> {value("Mat4")} </div>
         <div onClick={() => setSelectTrue("Mat5")}> {value("Mat5")} </div>
-        
+        <div onClick={() => setSelectTrue("Mat6")}> {value("Mat6")} </div>
+        <div onClick={() => setSelectTrue("Mat7")}> {value("Mat7")} </div>
+        <div onClick={() => setSelectTrue("Mat8")}> {value("Mat8")} </div>
+        <div onClick={() => setSelectTrue("Mat9")}> {value("Mat9")} </div>
+        <div onClick={() => setSelectTrue("Mat10")}> {value("Mat10")} </div>
     </div>
   );
 }
