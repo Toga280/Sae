67365b3e439e4e71a6e1006a75c2c52cab4032a5
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-01-08 14:13:26 +0100

    fix css add & create user + set perms prof

--- frontend/src/Component/Interface_Educateur/Interface.tsx
+++ frontend/src/Component/Interface_Educateur/Interface.tsx
@@ -23,7 +23,7 @@ function Interface({ redirection, role }: any) {
             <button className="bouton_interface_educateur" onClick={() => redirection(14)} >Consulter fiches/images</button>
             <button className="bouton_interface_educateur" onClick={() => redirection(10)} > Importer un pictogramme</button>
             {/* <button className="bouton_interface_educateur" onClick={() => redirection(3)}>Brouillons fiches </button> */}
-            <button className="bouton_interface_educateur" onClick={() => redirection(6)}>Fiches crées</button>
+            <button className="bouton_interface_educateur" onClick={() => redirection(6)}>Fiches créées</button>
             <button className="bouton_interface_educateur" onClick={() => redirection(9)}>Modifier mots de passe</button>
             <button className="bouton_interface_educateur" onClick={() => redirection(5)}>Créer un profil</button>
             <button className="bouton_interface_educateur" onClick={() => redirection(8)}>Archiver un profil</button>
