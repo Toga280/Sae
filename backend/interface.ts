296a95ff1ee0bc392430d9b64cc18067df8abb4e
REN-22 <renzo.bastide@gmail.com>
2024-01-11 16:42:03 +0100

    FIX

--- backend/interface.ts
+++ backend/interface.ts
@@ -54,6 +54,7 @@ export interface Admin{
   mdp: string;
   id: string;
   role: string;
+  
 }
 
 export interface Eleve{
@@ -62,6 +63,7 @@ export interface Eleve{
   image: string,
   mdp: string,
   archiver: boolean,
+  wallpaper: string
 }
 
 export interface Materiel {
