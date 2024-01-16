ec630799188dd9155c5818182bcd99c63abe9541
Enzo <enzo.delacour@etu.iut-tlse3.fr>
2024-01-16 22:07:56 +0100

    ajout sauvegarder

--- frontend/src/Component/CreationFiche/Sauvegarder.tsx
+++ frontend/src/Component/CreationFiche/Sauvegarder.tsx
@@ -8,6 +8,12 @@ function Sauvegarder({ redirection, setSaveName }: any) {
 
   const [nomFiche, setNomFiche] = useState("");
   const [upPopUpSauvegarder, setUpPopUpSauvegarder] = useState(false);
+  const [typeFiche, setTypeFiche] = useState("");
+  const [boutonClique, setBoutonClique] = useState(false);
+
+  const handleTypeSelection = (type : any) => {
+    setTypeFiche(type);
+  };
 
   const handleInputChange = (event: any) => {
     setNomFiche(event.target.value);
@@ -56,57 +62,87 @@ function Sauvegarder({ redirection, setSaveName }: any) {
 
   return (
     <div>
+      <div className="global_sauvegarder_fiche">
+        <h1 className="title_choose_name_for_fiche">Choisir un nom pour votre fiche</h1>
 
-    <div className="global_sauvegarder_fiche">
-      <h1 className="title_choose_name_for_fiche">Choisir un nom pour votre fiche</h1>
-      
-      <h5 className="titleCommentaire">Nom de la fiche</h5>
-      
-      <input
-        className="input_sauvegarder_ficheBox"
-        type="text"
-        placeholder="Nom de la fiche"
-        value={nomFiche}
-        required
-        onChange={handleInputChange}
-      />
-
-      <div className="commentaire">
-        <h5 className="titleCommentaire">Commentaire</h5>
-        <textarea
-          className="input_commenter_ficheBox"
-          placeholder="Commentaire"
+        <h2 className="titleCommentaire">Nom de la fiche</h2>
+        <input
+          className="input_sauvegarder_ficheBox"
+          type="text"
+          placeholder="Nom de la fiche"
+          value={nomFiche}
+          required
+          onChange={handleInputChange}
         />
+ <h2 className="titleCommentaire">Type de la fiche</h2>
+              <div>
+                <input
+                  type="button"
+                  className="boutton_type_ficheBox"
+                  value="Électricité"
+                  onClick={() => {
+                    handleTypeSelection('Électricité');
+                    setBoutonClique(true);
+                  }}
+
+                />
+                <input
+                  type="button"
+                  className="boutton_type_ficheBox"
+                  value="Général"
+                  onClick={() => {
+                    handleTypeSelection('Général');
+                    setBoutonClique(true);
+                  }}
+                />
+                <input
+                  type="button"
+                  className="boutton_type_ficheBox "
+                  value="Plomberie"
+                  onClick={() => {
+                    handleTypeSelection('Plomberie');
+                    setBoutonClique(true);
+                  }}
+                />
+              </div>
+
+        {boutonClique && (
+          <div>
+            <div className="commentaire">
+              <h2 className="titleCommentaire">Commentaire</h2>
+              <textarea
+                className="input_commenter_ficheBox"
+                placeholder="Commentaire"
+              />
+            </div>
+
+            <input
+              type="button"
+              className="boutton_sauvegarder_ficheBox"
+              value="Sauvegarder"
+              required
+              onClick={() => {
+                if (nomFiche.trim() !== "") {
+                  testNameFiche(nomFiche);
+                } else {
+                  alert("Veuillez saisir un nom pour votre fiche avant de sauvegarder.");
+                }
+              }}
+            />
+
+            {upPopUpSauvegarder ? (
+              <PopUpSauvegarder
+                setUpPopUpSauvegarder={setUpPopUpSauvegarder}
+                nomFiche={nomFiche}
+                sauvegarde={sauvegarde}
+              />
+            ) : null}
+          </div>
+        )}
       </div>
-      
-      <input
-        type="button"
-        className="boutton_sauvegarder_ficheBox"
-        value="Sauvegarder"
-        required
-        onClick={() => {
-          if (nomFiche.trim() !== "") {
-            testNameFiche(nomFiche);
-          } else {
-            alert("Veuillez saisir un nom pour votre fiche avant de sauvegarder.");
-          }
-        }}
-      />
-
-      
-
-      {upPopUpSauvegarder ? (
-        <PopUpSauvegarder
-          setUpPopUpSauvegarder={setUpPopUpSauvegarder}
-          nomFiche={nomFiche}
-          sauvegarde={sauvegarde}
-        />
-      ) : null}
-
-    </div>
-    <button className="retour_btn_save" onClick={() => setSaveName(false)}>Retour</button>
+      <button className="retour_btn_save" onClick={() => setSaveName(false)}>Retour</button>
     </div>
   );
-}
+};
 
 export default Sauvegarder;
