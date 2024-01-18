57247c63dcbcfd3a684d13697d0be6e6a7a255d0
REN-22 <renzo.bastide@gmail.com>
2024-01-18 11:49:37 +0100

    refresh liste fiche last man standig

--- frontend/src/Component/FicheCreer/ListeFiches.tsx
+++ frontend/src/Component/FicheCreer/ListeFiches.tsx
@@ -12,28 +12,32 @@ function ListeFiches({ redirection, refreshFiche }: any) {
     setAffichageAffecterListe(false);
   };
   const deleteFiche = (nomFiche: string) => {
-    const confirmation = window.confirm(
-      `Êtes-vous sûr de vouloir supprimer cette fiche ${nomFiche} ?`
-    );
+  const confirmation = window.confirm(
+    `Êtes-vous sûr de vouloir supprimer cette fiche ${nomFiche} ?`
+  );
 
-    if (confirmation) {
-      axios
-        .get(
-          `http://localhost:5000/DELETE/ficheName?name=${encodeURIComponent(
-            nomFiche
-          )}`
-        )
-        .then((response) => {
-          if (response.data) {
-            console.log("fiche supprimée avec succès");
-            allFicheNames();
-          }
-        })
-        .catch((error) => {
-          console.error("Erreur lors de la requête vers le serveur :", error);
-        });
-    }
-  };
+  if (confirmation) {
+    axios
+      .get(
+        `http://localhost:5000/DELETE/ficheName?name=${encodeURIComponent(
+          nomFiche
+        )}`
+      )
+      .then((response) => {
+        if (response.data) {
+          console.log("fiche supprimée avec succès");
+          // Mettre à jour l'état après la suppression
+          setFichesNames((prevFiches) => {
+            const updatedFiches = prevFiches.filter((fiche) => fiche !== nomFiche);
+            return updatedFiches;
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Erreur lors de la requête vers le serveur :", error);
+      });
+  }
+};
 
   const affecterFiche = (item: string) => {
     console.log(item);
